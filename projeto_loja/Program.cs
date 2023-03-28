805e66be42e13a2b97fd718bd2b232355a349423
Nicolas Garcia <100208523+ProjetosNicolasGarcia@users.noreply.github.com>
2023-03-28 12:34:52 -0300

    Add files via upload

---
+++ projeto_loja/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace projeto_loja;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Produto p1 = new Produto();
+        // Produto p1 = new Produto("xyz");
+            Produto p1 = new Produto("abc",10);
+
+        p1.MostraDados();
+    }
+}
