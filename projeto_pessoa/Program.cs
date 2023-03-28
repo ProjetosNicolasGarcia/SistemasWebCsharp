1ee414c81ab2379758d04c2d6f30d62f5685d179
Nicolas Garcia <100208523+ProjetosNicolasGarcia@users.noreply.github.com>
2023-03-28 12:32:07 -0300

    Add files via upload

---
+++ projeto_pessoa/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace projeto_pessoa;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa("GODOFREDO",18);
+       //p1.AlteraNome("Nicolas");
+        //p1.AlteraSobreNome("Garcia");
+        //p1.AlteraIdade(16);
+        p1.MostraDados();
+
+    }
+}
