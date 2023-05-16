cae377710b90412f77c8be11245d7303cf007b17
Nicolas Garcia <100208523+ProjetosNicolasGarcia@users.noreply.github.com>
2023-05-16 11:59:35 -0300

    Add files via upload

--- exercicio02/ContratoPessoaFisica.cs
+++ exercicio02/ContratoPessoaFisica.cs
@@ -2,7 +2,14 @@
 class ContratoPessoaFisica:Contrato
 {
 
+   public long Cpf {get;set;}
 
-  
+   public int Idade {get;set;}
+
+    public override string MostraDados(){
+          string Tudo = this.Nome + "-" + this.Email + "-" + this.Telefone + "-" + this.Idade + "-" + this.Cpf;
+          return Tudo;
+    }
+
 }
 
--- exercicio02/Program.cs
+++ exercicio02/Program.cs
@@ -7,15 +7,19 @@ class Program
         ContratoPessoaFisica cf = new ContratoPessoaFisica();
         ContratoPessoaJuridica cj = new ContratoPessoaJuridica();
 
-        cf.Tipo = "Pessoa física";
+        cf.Nome = "Nicolas";
+        cf.Email = "nicolas@gmail.com";
+        cf.Telefone = "40028922";
         cf.Idade = 69;
         cf.Cpf = 44149248800;
-        cj.Tipo = "Pessoa Jurídica";
+        cj.Nome = "ETEC";
+        cj.Email = "etec@gmail.com";
+        cj.Telefone = "77776666";
         cj.Cnpj = 42520353000162;
         cj.Inscricao = 037689759148;
 
-        Console.WriteLine("Tipo de contrato: " + cf.Tipo + " -- Idade: " + cf.Idade + " -- CPF: " +cf.Cpf);
-        Console.WriteLine("Tipo de contrato: " + cj.Tipo + " -- CNPJ: " + cj.Cnpj + " -- Inscrição estadual: " +cj.Inscricao);
+        Console.WriteLine(cf.MostraDados());
+        Console.WriteLine(cj.MostraDados());
     }
 } 
 
